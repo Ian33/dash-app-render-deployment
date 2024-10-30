41269b1df2172331364f80750889ebb925bc7e7a
Ian H. <ianrhiggins@gmail.com>
2024-10-30 16:58:27 +0000

    updated callbacks

--- app.py
+++ app.py
@@ -20,12 +20,20 @@ app.layout = html.Div([
     html.H1("Battery Voltage Status of Sites"),
     dcc.Graph(id='battery-graph'),
     html.Button('Refresh Data', id='refresh-button', n_clicks=0),
+    dcc.Store(id = 'metadata'), # store site metadata
+    dcc.Store(id = 'gagers'), # store gager list
+    dcc.Store(id = 'telemetry'), # store telemetry
+
 ])
-
 # Define the layout of the dashboard
-
+@app.callback(
+    Output('metadata', 'data'),
+    Output('gagers', 'data'),
+    Input('refresh-button', 'n_clicks')
+)
 # Define functions as in your original code
-def site_metadata():
+def site_metadata(n_clicks):
+    """reads site meta data, returns sites and gager list"""
     socrata_api_id = "37ja57noqzsdkkeo5ox34pfzm"
     socrata_api_secret = "4i1u1tyb6mfivhnw2fqhhsrim675gurrw8g1zegdwomix9xj91"
     socrata_database_id = "g7er-dgc7"
@@ -38,13 +46,20 @@ def site_metadata():
     if response.status_code == 200:
         data = response.json()
         df = pd.DataFrame(data)
+       
         gager_list = df["gager"].drop_duplicates().tolist()
+        df = df.to_json(orient="split")
     else:
         print(f"Failed to retrieve data: {response.status_code}")
-        df, gager_list = pd.DataFrame(), []
+        df, gager_list = pd.DataFrame().to_json(orient="split"), []
     return df, gager_list
 
-def telemetry_status():
+
+@app.callback(
+    Output('telemetry', 'data'),
+    Input('refresh-button', 'n_clicks')
+)
+def telemetry_status(n_clicks):
     socrata_api_id = "37ja57noqzsdkkeo5ox34pfzm"
     socrata_api_secret = "4i1u1tyb6mfivhnw2fqhhsrim675gurrw8g1zegdwomix9xj91"
     socrata_database_id = "gzfg-8xtp"
@@ -66,15 +81,28 @@ def telemetry_status():
     if response.status_code == 200:
         data = response.json()
         df = pd.DataFrame(data)
+        df = df.to_json(orient="split")
     else:
         print(f"Failed to retrieve data: {response.status_code}")
         df = pd.DataFrame()
+        df = df.to_json(orient="split")
     return df
 
-def merge_info(metadata, status):
-    return metadata.merge(status, on="site")
 
-def create_battery_graph(battery_site_status):
+@app.callback(
+    Output('battery-graph', 'figure'),
+    Input('metadata', 'data'),
+    Input('telemetry', 'data'),
+    Input('refresh-button', 'n_clicks')
+)
+def create_battery_graph(metadata, telemetry, n_clicks):
+    metadata = pd.read_json(metadata, orient="split")
+    
+    telemetry = pd.read_json(telemetry, orient="split")
+  
+    battery_site_status = metadata.merge(telemetry, on="site")
+
+
     battery_site_status["longitude"] = battery_site_status["longitude"].astype(float)
     battery_site_status["latitude"] = battery_site_status["latitude"].astype(float)
     battery_site_status["battery_volts"] = battery_site_status["battery_volts"].astype(float)
@@ -101,19 +129,6 @@ def create_battery_graph(battery_site_status):
                           hover_name="site",
                           hover_data={"battery_volts": True, "latitude": False, "longitude": False, "color_category": False},
                           zoom=9)
-    return fig
-
-
-
-@app.callback(
-    Output('battery-graph', 'figure'),
-    Input('refresh-button', 'n_clicks')
-)
-def update_graph(n_clicks):
-    metadata, gager_list = site_metadata()
-    status = telemetry_status()
-    battery_site_status = merge_info(metadata, status)
-    fig = create_battery_graph(battery_site_status)
     return fig
 
 
